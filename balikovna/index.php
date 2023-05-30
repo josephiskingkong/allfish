f73bcc462c3d6e83c4a6afe22b83f24a272a079f
josephiskingkong <iloveuyungcole@gmail.com>
2023-05-30 11:10:31 +0700

    d

--- balikovna/index.php
+++ balikovna/index.php
@@ -19,7 +19,7 @@ $dotenv->load();
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="/balikovna/balikovna.css">
-    <link rel="stylesheet" href="chat.css">
+    <link rel="stylesheet" href="/balikovna/chat.css">
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <title>Sledovat balík | Balíkovna</title>
 </head>
